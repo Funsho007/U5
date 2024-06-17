70b1fd2f67cfbc5f37f557119953d9c10252e0bc
Funsho007 <yinkaoke007@gmail.com>
2024-06-17 09:05:40 -0500

    Add index view to display all products

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'products/index'
+  # Define routes for products index and show actions
+  resources :products, only: [:index, :show]
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
